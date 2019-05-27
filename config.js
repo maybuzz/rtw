0fd402577bb28bcbb1cc5117ea7cc13e57e370ab
May <may.johansson@hva.nl>
2019-05-27 05:19:39 +0200

    heroku

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "0q2uzdng6qxFtqN3dyMeYg3Fr",
+  consumer_secret: "u5RIz2ozo9iMZTyxPbAtd6kTqNBpKpPngeKU2v2oCzHVzHgHwv",
+  access_token: "1068293376179605505-sifz1r8EgiLMcnvDhpJx7sWBKPpUsl",
+  access_token_secret: "y6UpeCKlnwrCOwwuqLdpki1zilORE2MAocPQqI8K7Zahn"
+}
